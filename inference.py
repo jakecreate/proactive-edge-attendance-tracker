b0d16473eeb7363c7f8f8fd7b382a05074248cd7
JakeCreate <developjake@gmail.com>
2026-03-04 14:50:41 -0800

    enable face extraction using SCRFD

---
+++ inference.py
@@ -0,0 +1,60 @@
+import cv2 as cv
+from PIL import Image
+
+import numpy as np
+import torch
+
+from scrfd import SCRFD, Threshold
+ 
+scrfd_model = SCRFD.from_path('./models/scrfd.onnx')
+threshold = Threshold(probability=0.4)
+cap = cv.VideoCapture(0)
+
+if not cap.isOpened():
+    print("| cannot open camera")
+    exit()
+
+while True:
+    # capture
+    ret, frame = cap.read()
+ 
+    if not ret:
+        print("| can't receive frame --> exiting ...")
+        break
+
+    # preprocessing
+    image = cv.cvtColor(frame, cv.COLOR_BGR2RGB)
+    faces = scrfd_model.detect(Image.fromarray(image),threshold=threshold)
+
+    if len(faces) == 0:
+        print('| no face detected')
+        continue
+
+    # filter biggest photo
+    max_idx = None
+    max_area = 0
+    for i, face in enumerate(faces):
+        ul = face.bbox.upper_left
+        lr = face.bbox.lower_right
+
+        area = (lr.x - ul.x) * (lr.y - ul.y)
+        if area > max_area:
+            max_area = area
+            max_idx = i
+
+    face = faces[max_idx]
+    ul = face.bbox.upper_left
+    lr = face.bbox.lower_right
+    ul_x, ul_y, lr_x, lr_y = int(ul.x), int(ul.y), int(lr.x), int(lr.y)
+
+    # draw box
+    color = (0, 255, 0) 
+    text = 'biggest face'
+    cv.rectangle(frame, (ul_x, ul_y), (lr_x, lr_y), color, 2)
+    cv.putText(frame, text, (ul_x, ul_y - 10), cv.FONT_HERSHEY_SIMPLEX, 0.5, color, 2)
+    cv.imshow('frame', image[ul_y:lr_y, ul_x:lr_x])
+    if cv.waitKey(1) == ord('q'):
+        break
+ 
+cap.release()
+cv.destroyAllWindows()
