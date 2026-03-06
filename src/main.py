9ff09ff91f98867549a1396a372a8372f892b523
JakeCreate <developjake@gmail.com>
2026-03-06 15:45:39 -0800

    adjust for src/main.py

--- src/main.py
+++ src/main.py
@@ -8,25 +8,25 @@ from scripts.core.model import MobileFacenet
 from scrfd import SCRFD, Threshold
 # import models
 mfn_model = MobileFacenet().to(device)
-checkpoint = torch.load('../models/mobile_face_net.ckpt', map_location=device)
+checkpoint = torch.load('models/mobile_face_net.ckpt', map_location=device)
 mfn_model.load_state_dict(checkpoint['net_state_dict'])
 mfn_model.eval()
 
-scrfd_model = SCRFD.from_path('../models/scrfd.onnx')
+scrfd_model = SCRFD.from_path('models/scrfd.onnx')
 print('SCRFD loaded')
 
 ## add student to database
 department = 'CSE'
 course = 'CS131'
 
-emb.live_capture_faces(dir_storage=f'../data/{department}.db',
+emb.live_capture_faces(dir_storage=f'data/{department}.db',
                        course_section=course,
                        scrfd_model=scrfd_model,
                        mfn_model=mfn_model)
 
 
 # train classifier (this specifies which course it will be take attendance)
-knn, le = emb.train_knn(dir_storage=f'../data/{department}.db',
+knn, le = emb.train_knn(dir_storage=f'data/{department}.db',
                         course_section=course)
 
 
